b14ad1e9ed34a2bdb1d6a5d11afae5ff2d3a5631
HNY_Badger <markelov-02@list.ru>
2024-01-02 14:31:37 +0400

    feat: finished isSameLength

--- src/arrays-tasks.js
+++ src/arrays-tasks.js
@@ -152,8 +152,9 @@ function getAverage(arr) {
  *    isSameLength(['orange', 'banana', 'cherry']) => true
  *    isSameLength(['cat', 'dog', 'elephant']) => false
  */
-function isSameLength(/* arr */) {
-  throw new Error('Not implemented');
+function isSameLength(arr) {
+  const first = arr[0];
+  return arr.every((cur) => cur.length === first.length);
 }
 
 /**
