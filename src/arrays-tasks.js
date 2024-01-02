89fab7d2ea79370e26cc94291c8396b566feb3b9
HNY_Badger <markelov-02@list.ru>
2024-01-03 00:59:38 +0400

    feat: finished findCommonElements

--- src/arrays-tasks.js
+++ src/arrays-tasks.js
@@ -515,8 +515,13 @@ function getMaxItems(arr, n) {
  *    findCommonElements(['a', 'b', 'c'], ['b', 'c', 'd']) => [ 'b', 'c' ]
  *    findCommonElements([1, 2, 3], ['a', 'b', 'c']) => []
  */
-function findCommonElements(/* arr1, arr2 */) {
-  throw new Error('Not implemented');
+function findCommonElements(arr1, arr2) {
+  return arr1.reduce((prev, cur) => {
+    if (arr2.includes(cur)) {
+      prev.push(cur);
+    }
+    return prev;
+  }, []);
 }
 
 /**
