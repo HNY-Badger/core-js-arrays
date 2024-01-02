8df322f2c777dc9fd6c0f7f7a2f10c2af8042eef
HNY_Badger <markelov-02@list.ru>
2024-01-03 02:06:22 +0400

    feat: finished shiftArray

--- src/arrays-tasks.js
+++ src/arrays-tasks.js
@@ -584,8 +584,21 @@ function propagateItemsByPositionIndex(arr) {
  *    shiftArray(['a', 'b', 'c', 'd'], -1) => ['b', 'c', 'd', 'a']
  *    shiftArray([10, 20, 30, 40, 50], -3) => [40, 50, 10, 20, 30]
  */
-function shiftArray(/* arr, n */) {
-  throw new Error('Not implemented');
+function shiftArray(arr, n) {
+  const shift = new Array(arr.length);
+  arr.map((v, i) => {
+    let index = 0;
+    if (i + n < 0) {
+      index = arr.length + (i + n);
+    } else if (i + n >= arr.length) {
+      index = i + n - arr.length;
+    } else {
+      index = i + n;
+    }
+    shift[index] = v;
+    return v;
+  });
+  return shift;
 }
 
 /**
