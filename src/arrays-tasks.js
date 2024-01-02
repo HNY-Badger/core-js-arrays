64a8c5b5293bba592e1c91cb611b0634076fe4cf
HNY_Badger <markelov-02@list.ru>
2024-01-03 01:20:57 +0400

    feat: finished findLongestIncreasingSubsequence

--- src/arrays-tasks.js
+++ src/arrays-tasks.js
@@ -535,8 +535,19 @@ function findCommonElements(arr1, arr2) {
  *    findLongestIncreasingSubsequence([3, 10, 2, 1, 20]) => 2
  *    findLongestIncreasingSubsequence([50, 3, 10, 7, 40, 80]) => 3
  */
-function findLongestIncreasingSubsequence(/* nums */) {
-  throw new Error('Not implemented');
+function findLongestIncreasingSubsequence(nums) {
+  let streak = 0;
+  let max = 0;
+  nums.reduce((prev, cur) => {
+    if (cur > prev) {
+      streak += 1;
+      max = streak > max ? streak : max;
+    } else {
+      streak = 1;
+    }
+    return cur;
+  }, 0);
+  return max;
 }
 
 /**
